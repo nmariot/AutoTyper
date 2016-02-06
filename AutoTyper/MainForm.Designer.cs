e0d70269e9db2bfb1e203dafcd67b862ff2487e7
Nicolas Mariot <nicolas.mariot@e-nigma.Fr>
2016-02-06 18:22:43 +0100

    Commit AutoTyper

---
+++ AutoTyper/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AutoTyper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.txtTextToType = new System.Windows.Forms.TextBox();
+            this.chkEnabled = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtTextToType
+            // 
+            this.txtTextToType.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTextToType.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTextToType.Location = new System.Drawing.Point(12, 35);
+            this.txtTextToType.Multiline = true;
+            this.txtTextToType.Name = "txtTextToType";
+            this.txtTextToType.Size = new System.Drawing.Size(434, 302);
+            this.txtTextToType.TabIndex = 0;
+            this.txtTextToType.Text = "for (int i = 0; i < 10; i++)\r\n{\r\n\tConsole.WriteLine(\"i = \" + i);\r\n}";
+            // 
+            // chkEnabled
+            // 
+            this.chkEnabled.AutoSize = true;
+            this.chkEnabled.Location = new System.Drawing.Point(12, 12);
+            this.chkEnabled.Name = "chkEnabled";
+            this.chkEnabled.Size = new System.Drawing.Size(65, 17);
+            this.chkEnabled.TabIndex = 1;
+            this.chkEnabled.Text = "Enabled";
+            this.chkEnabled.UseVisualStyleBackColor = true;
+            this.chkEnabled.CheckedChanged += new System.EventHandler(this.chkEnabled_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 349);
+            this.Controls.Add(this.chkEnabled);
+            this.Controls.Add(this.txtTextToType);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Typer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextToType;
+        private System.Windows.Forms.CheckBox chkEnabled;
+    }
+}
+
---
+++ AutoTyper/MainForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace AutoTyper
+{
+    /// <summary>
+    /// MainForm
+    /// </summary>
+    public partial class MainForm : Form
+    {        
+        #region Declarations
+        private AutoTyper _typer;
+        #endregion
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void chkEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkEnabled.Checked)
+            {
+                _typer = new AutoTyper();
+                _typer.StartTyping(txtTextToType.Text);                
+            }
+            else
+            {
+                _typer.StopTyping();                
+            }
+        }
+
+        /// <summary>
+        /// get the scan code relative to the char 'c'
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private static byte GetScanCode(char c)
+        {
+            int keyCode = (int)c;
+
+            // Letters
+            if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            {
+                return (byte)keyCode;
+            }
+
+            return (byte)keyCode;
+        }
+    }
+}
